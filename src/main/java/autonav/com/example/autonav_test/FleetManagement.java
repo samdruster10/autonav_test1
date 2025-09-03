94be11031b6d83cff24054be5d6161f470c52e44
samdruster10 <hsalmancs2@gmail.com>
2025-09-03 20:17:17 +0530

    First commit

---
+++ src/main/java/autonav/com/example/autonav_test/FleetManagement.java
@@ -0,0 +1,21 @@
+package autonav.com.example.autonav_test;
+
+public class FleetManagement {
+    private List<Vehicle> vehicles;
+
+    public FleetManagement() {
+    //     vehicles = new ArrayList<>();
+    // }
+
+    // public void addVehicle(Vehicle vehicle) {
+    //     vehicles.add(vehicle);
+    // }
+
+    // public void removeVehicle(Vehicle vehicle) {
+    //     vehicles.remove(vehicle);
+    // }
+
+    // public List<Vehicle> getVehicles() {
+    //     return vehicles;
+    }
+}
